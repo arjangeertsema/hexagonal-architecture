6f13b8ee414a7dd6dce620d52735989ed1de1df5
Arjan Geertsema <arjan@synion.nl>
2021-11-24 16:34:17 +0100

    added zeebe generator

--- Adapters/Adapters.Zeebe/generate.sh
+++ Adapters/Adapters.Zeebe/generate.sh
@@ -1,7 +1,7 @@
 # Set-up
 find . -type d -name "Generated" | xargs rm -rf
 
-docker run --rm --user "$(id -u):$(id -g)" -v "${PWD}:/local" camunda-community-hub/csharp-generator-cli generate \
+docker run --rm --user "$(id -u):$(id -g)" -v "${PWD}:/local" synionnl/csharp-generator-cli generate \
     -i https://www.synion.nl/blog/bizdevops/living-documentation/process.bpmn \
     -o /local/Generated \
     --namespace Adapters.Zeebe.Generated
