724e5e53be29dd8452fe819380756c68158d7534
Arjan Geertsema <arjan@synion.nl>
2021-11-29 14:19:43 +0100

    temporary added bpmn tests to zeebe adapter

--- Adapters/Adapters.Zeebe.Tests/Steps/ProcessStepDefintions.cs
+++ Adapters/Adapters.Zeebe.Tests/Steps/ProcessStepDefintions.cs
@@ -7,10 +7,7 @@ public class ProcessStepDefintions
 {
     private readonly IBPMNTestBuilder testBuilder;
     private readonly Random random;
-    private IBPMNTestBuilderStep1? step1;
-    private IBPMNTestBuilderStep1? step2;
-    private IBPMNTestBuilderStepGateway? stepGateway;
-
+    private IBPMNTestBuilderStep1? step1;    private IBPMNTestBuilderStepGateway? stepGateway;
     public ProcessStepDefintions(IBPMNTestBuilder testBuilder)
     {
         this.testBuilder = testBuilder ?? throw new System.ArgumentNullException(nameof(testBuilder));
@@ -27,16 +24,16 @@ public class ProcessStepDefintions
     [When(@"the bot answers the question with a probability greater or equal the (.*)% and smaller then (.*)%")]
     public void WhenTheBotAnswersTheQuestionWithAProbabilityGreaterOrEqualTheAndSmallerThen(int p0, int p1)
     {
-        this.step2.Should().BeNull();
-        this.step2 = IsNotNull(this.step1)
+        this.step1.Should().NotBeNull();
+        this.step1 = IsNotNull(this.step1)
             .SetState(CreateState(random.Next(p0, p1 - 1)));
     }
 
     [When(@"the bot answers the question with a probability smaller then (.*)%")]
     public void WhenTheBotAnswersTheQuestionWithAProbabilitySmallerThen(int p0)
     {
-        this.step2.Should().BeNull();
-        this.step2 = IsNotNull(this.step1)
+        this.step1.Should().NotBeNull();
+        this.step1 = IsNotNull(this.step1)
             .SetState(CreateState(random.Next(0, p0 - 1)));
     }
 
@@ -44,26 +41,27 @@ public class ProcessStepDefintions
     public void WhenTheBotAnswersTheQuestionWithAProbabilityGreaterOrEqualThen(int p0)
     {
         this.step1.Should().NotBeNull();
-        this.step2 = IsNotNull(this.step1)
+        this.step1 = IsNotNull(this.step1)
             .SetState(CreateState(random.Next(p0, 100)));
     }
 
     [When(@"the gateway with id ""(.*)"" is executed")]
     public void WhenTheGatewayWithIdIsExecuted(string p0)
     {
+        this.step1.Should().NotBeNull();
         this.stepGateway.Should().BeNull();
-        this.stepGateway = IsNotNull(this.step2)
+        this.stepGateway = IsNotNull(this.step1)
             .ExecuteGateway(p0);
     }
 
     [Then(@"the flow with id ""(.*)"" must be activated")]
     public void ThenTheFlowWithIdMustBeActivated(string p0)
     {
-        var test = IsNotNull(this.stepGateway)
+        var result = IsNotNull(this.stepGateway)
             .ExpectFlowActivated(p0)
-            .Build();
+            .Execute();
 
-        test.Execute().Should().BeTrue();
+        result.Should().BeTrue();
     }
 
     #region Private
