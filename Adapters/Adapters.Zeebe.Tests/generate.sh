adddee298c4733240031e72269e55822dc37b1d8
Arjan Geertsema <arjan@synion.nl>
2021-12-02 14:55:41 +0100

    typo

--- Adapters/Adapters.Zeebe.Tests/generate.sh
+++ Adapters/Adapters.Zeebe.Tests/generate.sh
@@ -1,3 +1,3 @@
 find . -type d -name "Generated" | xargs rm -rf
 mkdir Generated
-wget https://raw.githubusercontent.com/synionnl/website/${VERSION}/docs/blog/bizdevops/living-documentation/process.feature -O ./Generated
+wget https://raw.githubusercontent.com/synionnl/website/${VERSION}/docs/blog/bizdevops/living-documentation/process.feature -O ./Generated/process.feature
